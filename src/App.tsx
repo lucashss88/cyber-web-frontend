bd0032897bb24feff39f8c9c8a2df2c2b68db60c
danielacvmelo <danielacvmelo@gmail.com>
2025-08-29 20:01:46 -0300

    feat: implement footer component for desktop and mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import ProductsPage from './pages/ProductsPage';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      
+      <div className="flex flex-col min-h-screen">
         <Header />
-        <Routes>
-          <Route path='/products_page' element={<ProductsPage />} />
-        </Routes>
+        
+        <main className="flex-grow">
+          <div className="container mx-auto p-4">
+            <Routes>
+              <Route path='/products_page' element={<ProductsPage />} />
+              
+            </Routes>
+          </div>
+        </main>
+        
+        
+        <Footer />
       </div>
     </BrowserRouter>
   );
