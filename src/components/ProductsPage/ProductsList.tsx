2a00a0d5087d9c0fc48043c53a8c245183658c72
Sandro-A-Moraes <sandro.corporativo3004@gmail.com>
2025-08-29 16:01:04 -0300

    feat: add color 6c in index.file

--- src/components/ProductsPage/ProductsList.tsx
+++ src/components/ProductsPage/ProductsList.tsx
@@ -4,7 +4,7 @@ import ProductCard from './ProductCard'
 const ProductsList = () => {
   return (
     <div
-    className='bg-red-500 w-86 m-auto h-auto'
+    className=' w-86 m-auto h-auto'
     >
         <ProductCard/>
     </div>
