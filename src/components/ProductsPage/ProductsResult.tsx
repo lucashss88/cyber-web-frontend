afa27ab08638eb22419d67e5fb3305d97974860f
Sandro-A-Moraes <sandro.corporativo3004@gmail.com>
2025-08-29 16:02:31 -0300

    fix: change text-d4 to text-6c in ProductsResult component

--- src/components/ProductsPage/ProductsResult.tsx
+++ src/components/ProductsPage/ProductsResult.tsx
@@ -4,7 +4,7 @@ const ProductsResult = () => {
     const [productCount, setProductCount] = useState<number>(0);
 
   return (
-    <div className='w-full text-d4 text-lg flex justify-start items-center gap-1'>
+    <div className='w-full text-6c text-lg flex justify-start items-center gap-1'>
         <p>ProductsResult :</p>
         <span className='font-bold text-black text-xl '>{productCount}</span>
         
