3279fadf851ba0bea9e69fd31a8b40b9a1510791
danielacvmelo <danielacvmelo@gmail.com>
2025-08-30 15:04:16 -0300

    (FE-D14000) feat: create reusable Breadcrumb component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import ProductsPage from './pages/ProductsPage';
+import ProductDetailsPage from './pages/ProductDetailsPage';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <div className="container mx-auto p-4">
             <Routes>
               <Route path='/products_page' element={<ProductsPage />} />
+              <Route path='/product/:productId' element={<ProductDetailsPage />} />
               
             </Routes>
           </div>
