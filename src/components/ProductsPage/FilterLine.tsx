bdc7557168ffadd06dc2bd525de72ffe8973553d
Sandro-A-Moraes <sandro.corporativo3004@outlook.com>
2025-09-01 16:13:32 -0300

    feat: add toggle function that shows mobile filter

--- src/components/ProductsPage/FilterLine.tsx
+++ src/components/ProductsPage/FilterLine.tsx
@@ -2,9 +2,15 @@ import {useState} from 'react'
 import filterIcon from '../../assets/images/productsPage/filterIcon.png'
 import byPriceIcon from '../../assets/images/productsPage/byPriceIcon.png'
 import type {SortOption} from '../../types/byPrice'
+import MobileFilter from './MobileFilter'
 
 const FilterLine = () => {
   const [order, setOrder] = useState<SortOption>("highToLow")
+  const [showMobileFilter, setShowMobileFilter] = useState<boolean>(false)
+
+  const toggleMobileFilter = () => {
+    setShowMobileFilter(prev => !prev)
+  }
 
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const value = e.target.value as SortOption
@@ -14,12 +20,13 @@ const FilterLine = () => {
   }
 
   return (
-    <div className="bg-white py-11 flex justify-center items-center">
+    <div className="bg-white py-11 flex justify-center items-center relative">
         <div className="bg-white w-86 flex justify-around items-center" id='filter-line-container'>
-            <div className="bg-white border border-d4 filter-box flex items-center justify-around min-w-41 min-h-14 px-2 rounded-md cursor-pointer">
+            <div className="bg-white border border-d4 filter-box flex items-center justify-around min-w-41 min-h-14 px-2 rounded-md cursor-pointer" onClick={toggleMobileFilter}>
                 <span className='text-sm font-normal'>Filters</span>
                 <img src={filterIcon} alt="filter-icon" className='w-6'/>
             </div>
+            {showMobileFilter && <MobileFilter onClose={toggleMobileFilter}/>}
 
             <div className="bg-white border border-d4 filter-box flex items-center justify-around min-w-41 min-h-14 px-2 rounded-md relative">
                 <select className='flex-1 text-xs bg-transparent outline-none appearance-none font-normal text-gray-700' name="byPrice" id="byPriceSelect" value={order} onChange={handleChange}>
