31a0a5c16963588d23c7aab5ef36815a4d3f3f38
Vitor Andre Beckenkamp <vitorandrebeckenkamp@outlook.com>
2025-09-01 17:56:02 -0300

    patch: removed MainLayout Function on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,34 +5,31 @@ import Footer from './components/Footer';
 import ProductsPage from './pages/ProductsPage';
 import ProductDetailsPage from './pages/ProductDetailsPage';
 import HomePage from './pages/HomePage';
-
-function MainLayout({children}: {children: React.ReactNode}) {
-  return (
-    <main className="flex-grow">
-      <div className="container mx-auto p-4">
-        {children}
-      </div>
-    </main>
-  );
-}
-
+ 
 function App() {
   return (
     <BrowserRouter>
-      
+     
       <div className="flex flex-col min-h-screen">
         <Header />
-        
-        <Routes>
-          <Route path='/products_page' element={<MainLayout><ProductsPage /></MainLayout>} />
-          <Route path='/product/:productId' element={<MainLayout><ProductDetailsPage /></MainLayout>} />
-          <Route path='/home' element={<HomePage />}/>    
-        </Routes>
-        
+       
+        <main className="flex-grow">
+         
+            <Routes>
+              <Route path='/products_page' element={<ProductsPage />} />
+              <Route path='/product/:productId' element={<ProductDetailsPage />} />
+              <Route path='/home' element={<HomePage />} />
+             
+            </Routes>
+         
+        </main>
+       
+       
         <Footer />
       </div>
     </BrowserRouter>
   );
 }
-
+ 
 export default App;
+ 
