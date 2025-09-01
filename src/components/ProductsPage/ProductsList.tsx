5b11b5c38452b7e7970db9d9ed840ba1ebf82efc
Sandro-A-Moraes <sandro.corporativo3004@outlook.com>
2025-09-01 00:55:27 -0300

    feat: add Pagination element in ProductsList

--- src/components/ProductsPage/ProductsList.tsx
+++ src/components/ProductsPage/ProductsList.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import {useState} from 'react'
 import ProductCard from './ProductCard'
 import ProductsResult from './ProductsResult'
+import Pagination from './Pagination'
 
 const ProductsList = () => {
+  const [page, setPage] = useState(1)
+
   return (
     <div
-    className=' w-86 m-auto h-auto flex flex-col'
+    className=' w-86 m-auto h-auto flex flex-col items-center'
     >
         <ProductsResult/>
         <div className=' w-full h-auto flex gap-4 mt-5 flex-wrap'>
@@ -18,6 +21,8 @@ const ProductsList = () => {
           <ProductCard/>
           <ProductCard/>
         </div>
+
+        <Pagination currentPage={page} totalPages={12} onPageChange={setPage}/>
     </div>
   )
 }
