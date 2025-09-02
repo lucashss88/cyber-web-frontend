a45337af96d8a3b8a5ed83fe1b47fa197bb2de50
Vitor Andre Beckenkamp <vitorandrebeckenkamp@outlook.com>
2025-09-02 15:08:20 -0300

    feat: added discounted products section

--- src/components/Home/DiscountedProducts.tsx
+++ src/components/Home/DiscountedProducts.tsx
@@ -3,10 +3,10 @@
 export default function DiscountedProducts() {
 
     return (    
-        <div className="w-full bg-white py-10">
+        <div className="w-full bg-white py-10 px-3 sm:px-10">
             <div className="max-w-6xl mx-auto px-4 pr-4">
                 <div className="flex justify-between">
-                    <h2 className="text-2xl font-semibold mb-6">Discounted up to -50%</h2>            
+                    <h2 className="text-2xl font-medium mb-6">Discounted up to -50%</h2>            
                 </div>
             </div>
         </div>
