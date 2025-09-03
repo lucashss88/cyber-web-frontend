ec7903d94e001416b56de4d2e013ed399e99df09
Sandro-A-Moraes <sandro.corporativo3004@outlook.com>
2025-09-03 17:38:26 -0300

    feat: render ProductCard dinamically from backend data

--- src/components/ProductsPage/ProductCard.tsx
+++ src/components/ProductsPage/ProductCard.tsx
@@ -1,25 +1,32 @@
-import React from 'react'
 import heartIcon from '../../assets/images/header/heart-icon.png'
-import phoneImage from '../../assets/images/productsPage/phone-example.png'
 
+interface ProductCardProps {
+  name: string
+  price: number
+  imageUrl: string
+}
 
-const ProductCard = () => {
+const ProductCard = ({ name, price, imageUrl }: ProductCardProps) => {
   return (
-    <div className='bg-f6 w-41 h-88 py-5 rounded-[9px] flex flex-col items-center justify-around lg:w-56'>
-        <div className=' w-35 flex justify-end'>
-            <img src={heartIcon} alt="heart-icon" className='opacity-30 w-8'/>
+    <div className="w-41 h-88 py-5 rounded-[9px] flex flex-col items-center justify-around lg:w-56 lg:min-h-88">
+      <div className="w-35 flex justify-end">
+        <img src={heartIcon} alt="heart-icon" className="opacity-30 w-8" />
+      </div>
+      <div className="w-26 lg:w-28 flex justify-center">
+        <img src={imageUrl} alt={name} className="object-contain lg:w-23 lg:max-h-25" />
+      </div>
+      <div className="w-35 text-center flex flex-col gap-4 lg:w-full  items-center">
+        <div className=" lg:h-6">
+          <p className="font-bold text-black text-center line-clamp-2">
+            {name}
+          </p>
         </div>
-        <div className='w-26 lg:w-28'>
-            <img src={phoneImage} alt="phone-example-image" />
-        </div>
-        <div className='w-35 text-center flex flex-col gap-4 lg:w-45 items-center'>
-            <div>
-                <p className='font-bold text-black text-center line-clamp-2'>Apple iPhone 14 Pro 512GB Gold &#40;MQ233&#41;</p>
-            </div>
-            <span className='block font-bold text-2xl'>$1437</span>
+        <span className="block font-bold text-2xl lg:mt-3">${price}</span>
 
-            <button className='text-white text-sm w-35 h-12 bg-black rounded-[8px]'>Buy Now</button>
-        </div>
+        <button className="text-white text-sm w-35 h-12 bg-black rounded-[8px]">
+          Buy Now
+        </button>
+      </div>
     </div>
   )
 }
--- src/components/ProductsPage/ProductsList.tsx
+++ src/components/ProductsPage/ProductsList.tsx
@@ -1,13 +1,44 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import ProductCard from './ProductCard'
 import ProductsResult from './ProductsResult'
 import Pagination from './Pagination'
 import ByPrice from './ByPrice'
 import type { SortOption } from '../../types/byPrice'
 
+interface Product{
+  id: number
+  name: string
+  price: number
+  url_image: string
+}
+
 const ProductsList = () => {
   const [page, setPage] = useState(1)
   const [order, setOrder] = useState<SortOption>("highToLow")
+  const url = import.meta.env.VITE_API_URL
+  const [products, setProducts] = useState<Product[]>([])
+  const [totalPages, setTotalPages] = useState(1)
+
+  useEffect(()=>{
+    async function fetchProducts(){
+      try{
+        const response = await fetch(`${url}/api/products?page=${page}&order=${order}`)
+        const data = await response.json()
+        const productsData = data.data
+        console.log("API response:", data)
+        console.log("Fetched products:", productsData)
+
+        setProducts(productsData)
+        setTotalPages(data.metadata.total_pages)
+        console.log("Products state updated:", products)
+
+
+      } catch(e){
+        console.error("Error fetching products: ", e)
+      }
+    }
+    fetchProducts()
+  }, [url, page, order])
 
   return (
     <div className="w-19/20 lg:w-full m-auto h-auto flex flex-col items-center">
@@ -18,16 +49,15 @@ const ProductsList = () => {
       </div>
 
       <div className="w-full h-auto flex justify-center gap-4 mt-5 flex-wrap  lg:w-full lg:grid lg:grid-cols-3 lg:gap-4 lg:justify-items-center">
-
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            name={product.name}
+            price={product.price}
+            imageUrl={product.url_image}
+          />
+        ))}
+        
       </div>
 
       <Pagination currentPage={page} totalPages={12} onPageChange={setPage} />
