66f9be4692283e79bcf876c1e46f849967e8b921
Sandro-A-Moraes <sandro.corporativo3004@gmail.com>
2025-09-03 08:04:55 -0300

    feat: add Breadcrumb in products_page

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import FilterLine from '../components/ProductsPage/FilterLine'
 import ProductsList from '../components/ProductsPage/ProductsList'
+import Breadcrumb from '../components/Breadcrumb'
 
 const ProductsPage = () => {
   return (
     <>
+        <div className='w-19/20 mx-auto px-40 '>
+          <Breadcrumb crumbs={[{label: 'Home', href: '/'}, {label: 'Catalog', href: '/'}, {label: 'Smartphones', href: '/products_page'}]}/>
+        </div>
         <FilterLine/>
         <ProductsList/>
     </>
