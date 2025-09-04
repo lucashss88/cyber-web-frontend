64e21c374eb433e2ce67722c2a16ad9d9c5a1961
Vitor Andre Beckenkamp <vitorandrebeckenkamp@outlook.com>
2025-09-04 14:27:27 -0300

    feat: added products with discount from the backend in the discounted product section

--- src/components/Home/DiscountedProducts.tsx
+++ src/components/Home/DiscountedProducts.tsx
@@ -1,6 +1,52 @@
 'use client'
 
+import {useEffect, useState} from 'react';
+import ProductCard from '../productDetailsPage/relatedProducts/ProductCard';
+
+type ApiProduct = {
+    id: number,
+    name: string,
+    price: string,
+    tag: string,
+    url_image: string,
+};
+
+type ProductCardProps = {
+    imageUrl: string,
+    name: string,
+    price: number,
+};
+
 export default function DiscountedProducts() {
+
+    const [products, setProducts] = useState<ProductCardProps[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    
+    useEffect(() => {
+        const fetchProduct = async () => {
+            setLoading(true);
+            try {
+                const response = await fetch(`http://localhost:3001/api/products/tag/discount_up_to_50`);
+                const json = await response.json();
+
+                const mappedProducts: ProductCardProps[] = (json.data || []).map((item: ApiProduct) => ({
+                    imageUrl: item.url_image,
+                    name: item.name,
+                    price: parseFloat(item.price),
+                }));
+
+                setProducts(mappedProducts);
+            } catch (e) {
+                console.error("Erro ao buscar produtos com desconto: ", e);
+                setProducts([]);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchProduct();
+    }, []);
 
     return (    
         <div className="w-full bg-white py-10 px-3 sm:px-10">
@@ -9,6 +55,18 @@ export default function DiscountedProducts() {
                     <h2 className="text-2xl font-medium mb-6">Discounted up to -50%</h2>            
                 </div>
             </div>
+
+            {loading ? (
+                <div className='flex justify-center items-center h-200'>
+                    <div className="w-8 h-8 border-4 border-gray-300 border-t-black rounded-full animate-spin"></div>
+                </div>
+            ) : (
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 px-4 max-w-6xl mx-auto">
+                    {products.slice(0, 4).map((product, index) => (
+                        <ProductCard key={index} product={product} />
+                    ))}
+                </div>
+            )}
         </div>
     )
 }
