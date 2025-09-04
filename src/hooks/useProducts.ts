119c88b0f1d9adc163c61c2ea572fe9696e30e2d
Sandro-A-Moraes <sandro.corporativo3004@outlook.com>
2025-09-04 20:03:31 -0300

    feat: implement High to Low logic in useProducts hook

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,51 +1,49 @@
 import { useState, useEffect } from 'react'
-import type { SortOption } from '../types/byPrice'
+import type { SortOption } from '../types/byPrice' 
 
 interface Product {
-  id: number
-  name: string
-  price: number
-  url_image: string
+  id: number  
+  name: string  
+  price: number  
+  url_image: string 
 }
 
-interface UseProductsResult {
-  products: Product[]
-  totalProducts: number
+interface UseProductsResult {  
+  products: Product[]  
   totalPages: number
-  loading: boolean
-  error: string | null
+  loading: boolean  
+  error: string | null 
 }
 
 export function useProducts(page: number, order: SortOption): UseProductsResult {
-  const url = import.meta.env.VITE_API_URL
-  const [products, setProducts] = useState<Product[]>([])
-  const [totalPages, setTotalPages] = useState(1)
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState<string | null>(null)
-  const [totalProducts, setTotalProducts] = useState<number>(0)
+ const url = import.meta.env.VITE_API_URL
+ const [products, setProducts] = useState<Product[]>([])
+ const [totalPages, setTotalPages] = useState(1)
+ const [loading, setLoading] = useState(true)
+ const [error, setError] = useState<string | null>(null)  
+ 
+ useEffect(() => {
+   async function fetchProducts() {      setLoading(true)
+     setError(null)      //modifiquei aqui      
+     try {
+        const backendOrder = order === 'highToLow' ? 'desc' : 'asc';
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/api/products?page=${page}&order=${backendOrder}`);
 
-  useEffect(() => {
-    async function fetchProducts() {
-      setLoading(true)
-      setError(null)
-
-      try {
-        const response = await fetch(`${url}/api/products?page=${page}&order=${order}`)
-        const data = await response.json()
-
-        setProducts(data.data)
-        setTotalPages(data.metadata.total_pages)
-        setTotalProducts(data.metadata.total_items)
+        if (!response.ok) {
+         throw new Error(`O servidor respondeu com o status:${response.status}`);
+        }
+        const data = await response.json();
+        setTotalPages(data.metadata.total_pages);
+        setProducts(data.data);      
       } catch (e) {
-        setError("Error fetching products")
-        console.error("Error fetching products: ", e)
-      } finally {
-        setLoading(false)
-      }
-    }
-
+          setError("Error fetching products")
+          console.error("Error fetching products: ", e)      
+        } finally {
+          setLoading(false)      
+        }    
+    }    
+    
     fetchProducts()
-  }, [url, page, order])
-
-  return { products, totalPages, loading, error, totalProducts }
-}
+ }, [url, page, order])  
+ 
+return { products, totalPages, loading, error } }
