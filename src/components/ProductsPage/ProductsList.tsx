0deff3b475377a8fd14713f9710d2ed0af0890f1
Sandro-A-Moraes <sandro.corporativo3004@outlook.com>
2025-09-03 23:08:35 -0300

    feat: create useProducts hooks

--- src/components/ProductsPage/ProductsList.tsx
+++ src/components/ProductsPage/ProductsList.tsx
@@ -1,54 +1,28 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import ProductCard from './ProductCard'
 import ProductsResult from './ProductsResult'
 import Pagination from './Pagination'
 import ByPrice from './ByPrice'
 import type { SortOption } from '../../types/byPrice'
-
-interface Product{
-  id: number
-  name: string
-  price: number
-  url_image: string
-}
+import { useProducts } from '../../hooks/useProducts'
 
 const ProductsList = () => {
   const [page, setPage] = useState(1)
   const [order, setOrder] = useState<SortOption>("highToLow")
-  const url = import.meta.env.VITE_API_URL
-  const [products, setProducts] = useState<Product[]>([])
-  const [totalPages, setTotalPages] = useState(1)
 
-  useEffect(()=>{
-    async function fetchProducts(){
-      try{
-        const response = await fetch(`${url}/api/products?page=${page}&order=${order}`)
-        const data = await response.json()
-        const productsData = data.data
-        console.log("API response:", data)
-        console.log("Fetched products:", productsData)
-
-        setProducts(productsData)
-        setTotalPages(data.metadata.total_pages)
-        console.log("Products state updated:", products)
-
-
-      } catch(e){
-        console.error("Error fetching products: ", e)
-      }
-    }
-    fetchProducts()
-  }, [url, page, order])
+  const { products, totalPages, loading, error } = useProducts(page, order)
 
   return (
     <div className="w-19/20 lg:w-full m-auto h-auto flex flex-col items-center">
-      <div className=" lg:flex lg:w-full lg:justify-between">
+      <div className="lg:flex lg:w-full lg:justify-between">
         <ProductsResult />
-
         <ByPrice order={order} setOrder={setOrder} />
       </div>
 
-      <div className="w-full h-auto flex justify-center gap-4 mt-5 flex-wrap  lg:w-full lg:grid lg:grid-cols-3 lg:gap-4 lg:justify-items-center">
+      {loading && <p>Loading...</p>}
+      {error && <p className="text-red-500">{error}</p>}
+
+      <div className="w-full h-auto flex justify-center gap-4 mt-5 flex-wrap lg:w-full lg:grid lg:grid-cols-3 lg:gap-4 lg:justify-items-center">
         {products.map((product) => (
           <ProductCard
             key={product.id}
@@ -57,10 +31,9 @@ const ProductsList = () => {
             imageUrl={product.url_image}
           />
         ))}
-        
       </div>
 
-      <Pagination currentPage={page} totalPages={12} onPageChange={setPage} />
+      <Pagination currentPage={page} totalPages={totalPages} onPageChange={setPage} />
     </div>
   )
 }
--- src/components/ProductsPage/ProductsResult.tsx
+++ src/components/ProductsPage/ProductsResult.tsx
@@ -1,7 +1,8 @@
 import {useState} from 'react'
 
 const ProductsResult = () => {
-    const [productCount, setProductCount] = useState<number>(0);
+  const [productCount, setProductCount] = useState<number>(0);
+  
 
   return (
     <div className='w-full text-6c text-lg md:text-2xl flex justify-start items-center gap-1'>
