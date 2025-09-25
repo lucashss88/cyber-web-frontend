2a151e2a5cebb7bac8e1544031d322c8b2211368
lucashss88 <lucashss88@gmail.com>
2025-09-25 17:28:20 -0300

    feat(desktop): adding Step 1 page(address)

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Location from "../assets/images/checkout/Location.svg"
 import Payment from "../assets/images/checkout/Payment.svg"
 import Shippment from "../assets/images/checkout/Shipping.svg"
@@ -8,6 +8,17 @@ import PaymentCheckoutPage from "../components/Checkout/PaymentCheckoutPage"
 
 const CheckoutPage = () => {
   const [stepIndex, setStepIndex] = useState(0);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  
   const steps = [
     {
       step: 'Step 1',
@@ -42,24 +53,40 @@ const CheckoutPage = () => {
   }
 
   return (
-    <div className="flex flex-col md:flex-row gap-10 md:gap-0 md:justify-between p-4 w-full">
-      <div className="md:w-1/3">
-        <div className="flex gap-10">
-          {steps.slice(stepIndex, stepIndex + 2).map((step, index) => (
-            <div key={stepIndex + index} className="flex flex-col gap-1 justify-between py-4 m-auto">
-              <div className="flex items-center gap-2">
-                <img src={step.icon} alt={step.title} className="w-7 h-7" />
-                <div className="flex flex-col">
-                  <p className="text-sm font-semibold">{step.step}</p>
-                  <p className="text-lg font-semibold">{step.title}</p>
+    <div className="flex flex-col md:flex-row gap-10 md:gap-10 p-4 w-full md:pt-10">
+      <div className="md:w-full">
+        <div className="flex flex-row gap-10 md:pl-1 md:pr-20">
+          {isMobile ? (
+            <div>
+              {steps.slice(stepIndex, stepIndex + 2).map((step, index) => (
+                <div key={stepIndex + index} className="flex flex-col gap-1 justify-between py-4 m-auto">
+                  <div className="flex items-center gap-2">
+                    <img src={step.icon} alt={step.title} className="w-7 h-7" />
+                    <div className="flex flex-col">
+                      <p className="text-sm font-semibold">{step.step}</p>
+                      <p className="text-lg font-semibold">{step.title}</p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : (
+            steps.map((step, index) => (
+              <div key={stepIndex + index} className="flex flex-col gap-1 justify-between py-4 m-auto">
+                <div className="flex items-center gap-2">
+                  <img src={step.icon} alt={step.title} className="w-7 h-7" />
+                  <div className="flex flex-col">
+                     <p className="text-sm font-semibold">{step.step}</p>
+                    <p className="text-lg font-semibold">{step.title}</p>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))
+          )}
         </div>
-        <div className="flex flex-col gap-2 py-13">
+        <div className="flex flex-col gap-2 py-13 md:pl-35 md:pr-55 md:mt-12">
           {steps[stepIndex].content}
-          <div className="flex items-center w-full justify-between">
+          <div className="flex items-center w-full justify-between md:flex md:justify-end md:gap-5">
             <button className="bg-white border-2 border-black-500 text-black px-13 py-4 rounded-lg w-40" onClick={() => handlePreviousStep()}>Back</button>
             <button className="bg-black text-white px-13 py-4 rounded-lg w-40" onClick={() => handleNextStep()}>Next</button>
           </div>
