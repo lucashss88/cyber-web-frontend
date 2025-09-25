704e7ce3f34bc7e62ccc77ab12ce6984019b0f66
lucashss88 <lucashss88@gmail.com>
2025-09-25 11:29:36 -0300

    feat(mobile): shopping cart page - order summary section

--- src/pages/ShoppingCartPage.tsx
+++ src/pages/ShoppingCartPage.tsx
@@ -12,6 +12,8 @@ type Product = {
 
 const ShoppingCartPage = () => {
     const [products, setProducts] = useState<Product[]>([]);
+    const estimatedTax = 50;
+    const estimatedShipping = 29;
 
     useEffect(() => {
         const storedCart = localStorage.getItem('shoppingCart');
@@ -21,9 +23,13 @@ const ShoppingCartPage = () => {
         }
     }, []);
 
-    const totalPrice = useMemo(() => {
+    const subTotalPrice = useMemo(() => {
         return products.reduce((total: number, product: Product) => total + (product.price * product.quantity), 0);
     }, [products]);
+
+    const totalPrice = useMemo(() => {
+        return subTotalPrice + estimatedTax + estimatedShipping;
+    }, [subTotalPrice]);
 
     const handleRemoveFromCart = (productId: string) => {
         const updatedCart = products.filter(product => product.id !== productId);
@@ -59,8 +65,8 @@ const ShoppingCartPage = () => {
     };
 
     return (
-      <div className="container my-5 md:mx-40 md:my-30 p-4 md:flex md:flex-row">
-        <div className="">
+      <div className="container my-5 md:mx-40 md:my-30 p-4 md:flex md:flex-row flex flex-col gap-3">
+        <div>
           <h1 className="text-2xl font-bold mb-10">Shopping Cart</h1>
           {products.map((product, index) => (
             <div key={index} className="flex items-center justify-between mb-4 py-5 w-full border-b-d4 border-b-1">
@@ -80,10 +86,42 @@ const ShoppingCartPage = () => {
               </div>
             </div>
           ))}
+          {products.length === 0 && <p>Your cart is empty.</p>}
         </div>
-        <div className="">
-          <h1 className="text-2xl font-bold mb-4">Total</h1>
-          <p className="text-lg font-semibold">Total Price: ${totalPrice}</p>
+        <div className="py-10 px-2 mb-5">
+          <h1 className="text-2xl font-bold mb-5">Order Summary</h1>
+          <div className="flex flex-col gap-6 mb-2">
+            <div>
+              <p className="text-gray-400">Discount code / Promo code</p>
+              <input className="px-4 py-4 border-1 border-zinc-300 rounded-xl w-full" placeholder='Code'></input>
+            </div>
+            <div>
+              <p className="text-gray-400">Your bonus card Number</p>
+              <div className="relative">
+                <input className="px-4 py-4 pr-20 border-1 border-zinc-300 rounded-xl w-full" placeholder='Enter Card Number'></input>
+                <button className="absolute right-2 top-1/2 transform -translate-y-1/2 px-4 py-1 mr-1 text-sm border border-black rounded">Apply</button>
+              </div>
+            </div>
+          </div>
+          <div className="mb-2">
+            <div className="flex justify-between w-full mb-2">
+              <p className="text-lg font-semibold mt-5">Subtotal</p>
+              <p className="text-lg font-medium mt-5">${subTotalPrice}</p>
+            </div>
+            <div className="flex justify-between w-full">
+              <p className="text-lg font-normal mt-5">Estimated Tax</p>
+              <p className="text-lg font-medium mt-5">${estimatedTax}</p>
+            </div>
+            <div className="flex justify-between w-full">
+              <p className="text-lg font-normal mt-5">Estimated shipping & Handling</p>
+              <p className="text-lg font-medium mt-5">${estimatedShipping}</p>
+            </div>
+            <div className="flex justify-between w-full">
+              <p className="text-lg font-semibold mt-5">Total</p>
+              <p className="text-lg font-medium mt-5">${totalPrice}</p>
+            </div>
+          </div>
+          <button className="mt-10 w-full bg-black text-white py-4 rounded hover:bg-5d transition duration-300">Checkout</button>
         </div>
       </div>
     );
